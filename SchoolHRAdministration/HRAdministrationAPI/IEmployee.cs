f876c723c4a99212a7acb182202777ff03a89da6
Joseph Sung <chengyu.sung@gmail.com>
2024-02-08 21:19:04 +0800

    commit before pushing to github.

---
+++ SchoolHRAdministration/HRAdministrationAPI/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRAdministraionAPI
+{
+  public interface IEmployee
+  {
+    int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    decimal Salary { get; set; }
+  }
+}
---
+++ TestTimeConsumingOperationWebAPI/Controllers/TestLongOperationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTimeConsumingOperationWebAPI.Controllers
+{    
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(5000);
+            return Content("Web API Long Operation Completed");
+        }
+    }
+}
